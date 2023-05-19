eeeba4334e93d4f0e8d378fc34368357a56177a9
MF0-ANT1SHY <komeijiddz@gmail.com>
2023-05-20 00:42:45 +0800

    添加对地址值的解析功能

--- tokenizer.spec.ts
+++ tokenizer.spec.ts
@@ -4,8 +4,28 @@ import { TokenTypes,tokenizer } from "./tokenizer";
 test('paren',()=>{
     const code = "(";
     const tokens = [{
-        type: TokenTypes,
+        type: TokenTypes.Paren,
         value: "(",
+    }];
+
+    expect(tokenizer(code)).toEqual(tokens);
+})
+
+test('letter',()=>{
+    const code = "contract";
+    const tokens = [{
+        type: TokenTypes.Letter,
+        value: "contract",
+    }];
+
+    expect(tokenizer(code)).toEqual(tokens);
+})
+
+test('address',()=>{
+    const code = "0x123321123321123321";
+    const tokens = [{
+        type: TokenTypes.Address,
+        value: "0x123321123321123321",
     }];
 
     expect(tokenizer(code)).toEqual(tokens);
--- tokenizer.ts
+++ tokenizer.ts
@@ -1,5 +1,7 @@
 export enum TokenTypes {
     Paren,
+    Letter,
+    Address,
 }
 
 interface Token {
@@ -11,12 +13,53 @@ export function tokenizer(code: string) {
     const tokens: Token[] = [];
     let current = 0;
     let char = code[current];
+
+    /**
+     * parse (
+     */
     if (char === "(") {
         tokens.push({
-            type: TokenTypes,
+            type: TokenTypes.Paren,
             value: char,
         });
     }
 
+    /**
+     * parse letter
+     */
+    let LETTERS = /[a-z]/i;
+    if (LETTERS.test(char)){
+        let letter = "";
+        while( LETTERS.test(char) && current< code.length){
+            letter+=char;
+            char=code[++current];
+        }
+
+        tokens.push({
+            type:TokenTypes.Letter,
+            value: letter,
+        });
+    }
+
+    /**
+     * parse address
+     */
+    let NUMBERS = /[0-9]/
+    if(char==='0'&&code[current+1]==="x"){
+        let address = "";
+        address += char;
+        char = code[++current];
+        address += char;
+        char = code[++current];
+        while(NUMBERS.test(char) && current<code.length){
+            address+=char;
+            char=code[++current];
+        }
+
+        tokens.push({
+            type:TokenTypes.Address,
+            value:address,
+        });
+    }
     return tokens;
 }
